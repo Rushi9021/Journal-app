9d021882d544e5ee35627457f1a7b02a7b6074a1
Rushikesh Gaikwad <grushi5555@gmail.com>
2025-07-21 18:45:09 +0530

    init commit

---
+++ src/main/java/net/engineeringdigest/journalApp/Entity/JournalEntry.java
@@ -0,0 +1,18 @@
+package net.engineeringdigest.journalApp.Entity;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "journal_entries")
+@Data
+public class JournalEntry {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+}
